09927b7ad7ea73aa9472c755c1be3dada3e6300c
CRB404 <adamlukasik@adams-mbp-2.ideo.com>
2016-09-08 16:14:33 -0700

    Alert Modals Running
    Triggering modals with a different type of advance trigger on the prior
    page

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 (function($) {
 
-  var APP_STATES = ['sHook','sContext','sAccept','sEmail'];
+  var APP_STATES = ['sHook','sContext','sAccept','sMap','sAlert1','sEmail'];
   var INITIAL_STATE_IDX = 0
 
   var currentState = null
   var currentIdx = null
 
-  var variable = firebase.database().ref('emails');
+  var capture = firebase.database().ref('emails');
 
-  // Input Email Function
-  // _____________________________________________________________________________
+// Input Email Function
+// _____________________________________________________________________________
 
   function writeUserData() {
-    variable.push({ val: document.getElementById("search").value })
+    capture.push({ val: document.getElementById("search").value })
       .then(function() {
         console.log('Synchronization succeeded');
       })
@@ -20,6 +20,9 @@
         console.log('Synchronization failed');
       });
   }
+
+// Div Id Renderer | "Page Routing"
+// _____________________________________________________________________________
 
   // App helpers and initializers
   function hideAllStates() {
@@ -49,12 +52,31 @@
     setState(currentState)
   }
 
+  function runTimedModal() {
+    setTimeout(function() {
+      console.log('Modal is running')
+      $('#sAlert1').modal('show');
+      setTimeout(function() {
+        console.log('Modal closes')
+        $('#sAlert1').modal('hide');
+      }, 6000)
+    }, 10000)
+  }
+
   // APIs
   function advanceState() {
     hideState(currentState)
     currentIdx++;
     currentState = APP_STATES[currentIdx];
     showState(currentState);
+  }
+
+  function advanceToTransitState() {
+    hideState(currentState)
+    currentIdx++;
+    currentState = APP_STATES[currentIdx];
+    showState(currentState);
+    runTimedModal();
   }
 
   function complete() {
@@ -69,6 +91,7 @@
   // Expose the APIs
   window.App = {
     advance: advanceState,
+    advanceToTrasit: advanceToTransitState,
     complete: complete
   }
 
